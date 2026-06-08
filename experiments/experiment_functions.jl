41101fc696e3e74140a976bc69a0c7ca35b5c197
Thomas <thomas.bake@hotmail.com>
2026-06-08 12:43:09 +0200

    Fixed backward error scaling and implemented functions to write heatmap data

--- experiments/experiment_functions.jl
+++ experiments/experiment_functions.jl
@@ -1,10 +1,10 @@
 using MixedPrecisionPCG
-using Random, JSON3, BFloat16s
+using Random, JSON3, BFloat16s, JSON
 
 export geomdist_eigvalmatrix, low_precision_preconditioner, cond_experiment, upper_bound, 
-	   runpcgexperiments, strakos_mat, randsvd_spd, mat_prec, write_to_file
+	   runpcgexperiments, strakos_mat, mat_prec, write_to_file, write_heatmap_data
 
-export Experiment, Error, Residual, AbstractMetric
+export Experiment, Error, Residual, AbstractMetric, MinimaData, IndexData
 
 Random.seed!(1234)
 
@@ -12,6 +12,11 @@ abstract type AbstractMetric end
 
 struct Error    <: AbstractMetric end
 struct Residual <: AbstractMetric end
+
+abstract type AbstractHeatmapData end
+
+struct IndexData  <: AbstractHeatmapData end
+struct MinimaData <: AbstractHeatmapData end
 
 
 
@@ -66,43 +71,6 @@ function low_precision_preconditioner(A::AbstractMatrix; tol::AbstractFloat)
 
 end
 
-function cond_experiment(
-    scheme     ::Type{<:PreconditioningScheme},
-    n          ::Int,
-    precisions ::AbstractVector, 
-    tol        ::AbstractFloat,
-    kappa_range::Vector{Float64},
-    max_iter   ::Int)
-
-    ad_vector   = Vector{AccuracyData}(undef, length(kappa_range))
-
-    kappa_range_prec = similar(kappa_range)
-
-    x0 = zeros(n)
-
-    for i in eachindex(kappa_range)
-
-        # Create matrix with geometrically distributed eigenvalues.
-        A = geomdist_eigvalmatrix(kappa_range[i], n) 
-
-        x = rand(n)
-
-        b = A * x
-
-        ls = LinearSystem(A, b, x, x0)
-
-        # Create low precision Cholesky factor to use as preconditioner.
-        Lh = low_precision_preconditioner(A, tol = tol)
-
-        kappa_range_prec[i] = cond(Matrix(Lh))
-
-        ad_vector[i] = collect_data(scheme, precisions, ls, Lh, max_iter)
-
-    end
-
-    return ad_vector, kappa_range_prec
-end
-
 strakos_mat(n, λ1, λn, ρ) = diagm(vcat([λ1], [λ1 + (i - 1)/(n - 1) * (λn - λ1) * ρ^(n - i) for i in 2:n-1], [λn]))
 
 function mat_prec(n::Int, l1::Float64, ln::Float64, rho::Float64, cutoff::Int)
@@ -123,20 +91,20 @@ function mat_prec(n::Int, l1::Float64, ln::Float64, rho::Float64, cutoff::Int)
 
 end
 
-struct Experiment
+struct Experiment{T <: PreconditioningScheme} 
 	ls            ::LinearSystem
 	preconditioner::AbstractMatrix
 	max_iter      ::Integer
 	precisions    ::AbstractVector
 
-	function Experiment(
+	function Experiment{T}(
 			n         ::Integer,
 			lambda_min::AbstractFloat,
 			lambda_max::AbstractFloat,
 			rho       ::AbstractFloat,
 			i         ::Integer,
 			max_iter  ::Integer,
-			precisions::AbstractVector)
+			precisions::AbstractVector) where T <: PreconditioningScheme
 
 		A, L = mat_prec(n, lambda_min, lambda_max, rho, i) # Generate matrix and preconditioner.
 		b    = inv(sqrt(n)) .* ones(n)                     # Generate right-hand side.
@@ -147,19 +115,43 @@ struct Experiment
 	end
 end
 
-function runpcgexperiments(experiment::Experiment, scheme::Type{<:PreconditioningScheme})
+get_scheme(::Experiment{T}) where T = T
+
+function runpcgexperiments(experiment::Experiment{T}) where T <: PreconditioningScheme
 
     # Initialize AccuracyDataSeries data structure.
     ads = AccuracyDataSeries{Float64}(length(experiment.precisions), experiment.max_iter) 
 
-	collect_data!(
-	  ads,
-	  scheme,
-	  experiment.precisions,
-	  experiment.ls,
-	  experiment.preconditioner,
-	  experiment.max_iter
-	)
+    n  = size(experiment.ls.A, 1)
+
+	println("Preconditioning scheme: " * string(get_scheme(experiment)))
+
+	scheme = get_scheme(experiment)
+
+    # Iterate over different precision choices.
+    for i in eachindex(experiment.precisions)
+
+        # Extract precisions
+        uL, uR = getprecisions(scheme, experiment.precisions[i])
+
+        ML, MR = experiment.preconditioner, experiment.preconditioner'
+
+        # Generate preconditioner data structure with corresponding precisions.
+        M = FactorizationPreconditioner{uL, uR, scheme}(ML, MR)
+
+        # Initialize convergence data structure.
+        cd = ConvergenceData{Float64}(n, experiment.max_iter)
+
+        # Run PCG on linear system.
+        pcg!(cd, experiment.ls.A, M, experiment.ls.b, experiment.ls.x0, experiment.max_iter)
+
+        println("  Solved system " * string(i))
+
+        # Compute accuracy data (norms of true/updated residuals, errors, etc.)
+        compute_accuracy_data!(scheme, ads[i], cd, experiment.ls, experiment.preconditioner)
+
+    end
+
     return ads
 end
 
@@ -172,7 +164,7 @@ function upper_bound(
 	M = Symmetric(experiment.preconditioner * experiment.preconditioner')
 	A = experiment.ls.A
 	kappaM = cond(M)
-	kappaA = bound_type == Error ? cond(A) : 1.0
+	kappaA = (bound_type == Error) ? cond(A) : 1.0
 
     return u * sqrt(kappaM) * sqrt(kappaA)
 
@@ -196,12 +188,11 @@ get_metric_symbol(::Type{Error})    = :errornorm
 get_metric_symbol(::Type{Residual}) = :trueresnorm
 
 function write_to_file(
-		filename     ::String,
-		ads          ::AccuracyDataSeries,
-		bound        ::AbstractFloat,
-		experiment   ::Experiment,
-		scheme       ::Type{<:PreconditioningScheme},
-		metric       ::Type{<:AbstractMetric})
+		filename  ::String,
+		ads       ::AccuracyDataSeries,
+		bound     ::AbstractFloat,
+		experiment::Experiment,
+		metric    ::Type{<:AbstractMetric})
 
 	label_dict =  Dict(
 
@@ -211,17 +202,15 @@ function write_to_file(
         BFloat16 => "b16"
     )
 
-	processed_labels = [process_label(k, label_dict, scheme) for k in experiment.precisions]
+	processed_labels = [process_label(k, label_dict, get_scheme(experiment)) for k in experiment.precisions]
 
 	value_array   = zeros(experiment.max_iter)
 	metric_data   = Dict( label => copy(value_array) for label in processed_labels )
-	metric_symbol = get_metric_symbol(metric)
+	metric_symbol = get_metric_symbol(metric) # :errornorm or :trueresnorm
 
 	for (i, precision_label) in enumerate(processed_labels)
 
-		println(precision_label)
 		metric_data[precision_label] = getproperty(ads[i], metric_symbol)
-
 
 	end
 
@@ -234,5 +223,119 @@ function write_to_file(
 	end
 
 	JSON3.write(pwd() * "/json_data/" * filename, metric_data, allow_inf = true)
+
+end
+
+
+function transform_data_to_heatmap(experiment::Experiment, ads::AccuracyDataSeries)
+
+	metric_symbols = [:errornorm, :trueresnorm]
+
+	# Isolate data first in a vector. Dictionary with each value being a vector of vectors.
+	tmp_data = Dict(metric => [ getproperty(ads[i], metric) for i in eachindex(ads) ] for metric in metric_symbols )
+
+	
+	# Dictionary, where each value is a pair of floats and integers.
+	min_idx_pairs = Dict( 
+
+		metric => Vector{Tuple{Float64, Int}}(undef, experiment.max_iter) for metric in metric_symbols 
+
+	) 
+
+	# Apply Boolean mask and find corresponding minima and indices.
+	for metric in metric_symbols
+
+		tmp_data_metric = tmp_data[metric]
+
+		for (i, vector) in enumerate(tmp_data_metric)
+
+			masked_data_array = vector[ map(x -> x > 0.0, vector)]
+
+			min_idx_pairs[metric][i] = findmin(masked_data_array)
+
+		end
+
+	end
+
+	# We need to transform data to backward or forward error, respectively. 
+	normAx               = experiment.ls.normA * experiment.ls.normx
+	residual_multiplier  = normAx / ( norm(experiment.ls.b) * normAx )
+
+	multiplier = Dict( 
+
+				  metric 
+
+				  => metric == :trueresnorm ? residual_multiplier
+
+				  : sqrt(experiment.ls.normA) for metric in metric_symbols 
+				)
+
+	n_precisions    = 4 # Number of different precisions
+
+	minima_matrices = Dict(metric => zeros(n_precisions, n_precisions) for metric in metric_symbols)
+
+	iteration_count_matrix = Matrix{Int}(undef, (n_precisions, n_precisions))
+
+	for j in 1:n_precisions, i in 1:n_precisions
+
+		linear_index = i + (j - 1) * n_precisions
+
+		for metric in metric_symbols
+
+			min_metric_value             = min_idx_pairs[metric][linear_index][1]
+			minima_matrices[metric][j,i] = min_metric_value * multiplier[metric]
+
+		end
+
+		iteration_count_matrix[j,i] = min_idx_pairs[:errornorm][linear_index][2]
+
+	end
+
+	return minima_matrices, iteration_count_matrix
+
+end
+
+get_label_array(experiment::Experiment{Left}) = experiment.precisions
+
+function get_label_array(experiment::Experiment{Split}) 
+
+	n_precisions = length(experiment.precisions)
+
+	step = Int( sqrt(n_precisions) )
+
+	return [ experiment.precisions[i][1] for i in 1:step:n_precisions ]
+end
+
+function write_heatmap_data(filename::String, experiment::Experiment{T}, ads::AccuracyDataSeries) where T <: PreconditioningScheme
+
+	label_dict =  Dict(
+
+        Float64  => "fp64",
+        Float32  => "fp32",
+        Float16  => "fp16",
+        BFloat16 => "b16"
+    )
+
+	minima_matrices, iteration_count_matrix = transform_data_to_heatmap(experiment, ads)
+
+	label_array = get_label_array(experiment)
+
+	println(typeof(minima_matrices[:errornorm]))
+
+	data = Dict(
+			"FE_matrix"        => minima_matrices[:errornorm],
+			"BE_matrix"        => minima_matrices[:trueresnorm],
+			"ic_matrix"        => iteration_count_matrix,
+			"precision_labels" => [ label_dict[label] for label in label_array ]
+			)
+
+	if !isdir("json_data")
+
+		mkdir(pwd() * "/json_data")
+
+	end
+
+	#JSON3.write( pwd() * "/json_data/" * filename, data )
+	JSON.json( pwd() * "/json_data/" * filename, data )
 
 end
--- experiments/utils.jl
+++ experiments/utils.jl
@@ -1,5 +1,5 @@
 export FactPrec
-export getprecisions, collect_data!
+export getprecisions
 
 const FactPrec{uL, uR, S} = FactorizationPreconditioner{uL, uR, S}
 
@@ -14,42 +14,4 @@ function NumericalExperiments.getprecisions(::Type{<:AbstractSplit}, precisions:
     return precisions[1], precisions[2]
 end
 
-
-function collect_data!(
-    ads            ::AccuracyDataSeries,
-    scheme         ::Type{<:PreconditioningScheme},
-    precisions     ::AbstractVector,
-    ls             ::LinearSystem,
-    preconditioner ::AbstractMatrix,
-    max_iter::Int)
-
-    n  = size(ls.A, 1)
-
-    println("Preconditioning scheme: " * string(scheme))
-
-    # Iterate over different precision choices.
-    for i in eachindex(precisions)
-
-        # Extract precisions
-        uL, uR = getprecisions(scheme, precisions[i])
-
-        ML, MR = preconditioner, preconditioner'
-
-        # Generate preconditioner data structure with corresponding precisions.
-        M = FactorizationPreconditioner{uL, uR, scheme}(ML, MR)
-
-        # Initialize convergence data structure.
-        cd = ConvergenceData{Float64}(n, max_iter)
-
-        # Run PCG on linear system.
-        pcg!(cd, ls.A, M, ls.b, ls.x0, max_iter)
-
-        println("  Solved system " * string(i))
-
-        # Compute accuracy data (norms of true/updated residuals, errors, etc.)
-        compute_accuracy_data!(scheme, ads[i], cd, ls, preconditioner)
-
-    end
-
-end
 
